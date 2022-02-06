518553fe05d0a8b2ab174b5b17fe9a8879479177
DaengMa <qwq713@hotmail.com>
2022-02-06 10:56:04 +0900

    stop.sh 파일 수정
    - pid kill 이후 ppid 제거 추가

--- stop.sh
+++ stop.sh
@@ -9,10 +9,14 @@ if [ -z "$process_pid" ];
 then
   echo "$port_num is available"
 else
+  process_ppid=`ps -o ppid= -p $process_pid | tr -d '[:space:]'`
   echo "kill -9 $process_pid ..."
   kill -9 "$process_pid"
+  echo "kill -9 $process_ppid ..."
+  kill -9 "$process_ppid"
+
   sleep 3
   echo "$port_num is down, so $port_num is available"
 fi
 
-echo "===================== stop.sh Script Complete ====================="
+echo "===================== stop.sh Script Complete ============"
